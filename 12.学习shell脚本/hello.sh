f5674e3fb0a0323cb89cf315475e75b33a04c14c
Gookyn <liujinge.work@gmail.com>
2022-10-07 14:43:45 +0800

    feat: hello.sh

---
+++ 12.学习shell脚本/hello.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Program:
+#  This program shows "Hello World!" in your screen.
+# History:
+# 2022/10/07		Gookyn		First release
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+echo -e "Hello World!"
+
+exit 0
