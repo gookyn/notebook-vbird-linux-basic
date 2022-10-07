ee5a92e9af5dea8c4e796b0d14759cccdfe1acb5
Gookyn <liujinge.work@gmail.com>
2022-10-07 15:24:07 +0800

    feat: showname

---
+++ 12.学习shell脚本/showname.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Program:
+#   User inputs his first name and last name. Program shows his full name.
+# History:
+#   2022/10/07  Gookyn  first release
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+read -p "Please input your first name: " firstname
+read -p "Please input your last name: " lastname
+echo "Your full name is: ${firstname} ${lastname}"
